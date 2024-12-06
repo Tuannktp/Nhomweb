5a3476b43a2b89f26cf34b6cd89a7132d1344835
Thanh112k4 <169038464+Thanh112k4@users.noreply.github.com>
2024-12-06 18:28:21 +0700

    Update dashboard.php

---
+++ tlunews/views/admin/dashboard.php
@@ -0,0 +1,11 @@
+
+<?php include 'views/header.php'; ?>
+
+<div class="row">
+    <div class="col-md-12 mb-4">
+        <h2>Bảng Điều Khiển</h2>
+    </div>
+    
+    <?php include 'news/index.php' ?>
+
+<?php include 'views/footer.php'; ?>

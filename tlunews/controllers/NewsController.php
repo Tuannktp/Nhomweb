9a18932a96bde5a5b3a6929dc89bb42eaa85e523
Thanh112k4 <169038464+Thanh112k4@users.noreply.github.com>
2024-12-06 18:37:22 +0700

    Update NewsController.php

---
+++ tlunews/controllers/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../config/database.php';
+include_once '../models/News.php';
+include_once '../models/Category.php';
+
+class NewsController
+{
+    private $newsModel;
+    private $categoryModel;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->newsModel = new News($database->pdo);
+        $this->categoryModel = new Category($database->pdo);
+    }
+
+    public function getAllNews()
+    {
+        //For Dũng béo
+    }
+
+
+    public function detail($id)
+    {
+        
+    }
+
+}
+
+?>
